ce9acf8501f64fff9787a7f8678db7fb9a20f098
sepsupriatnaa <asep93339@gmail.com>
2024-08-27 20:31:30 +0700

    add image in gallery

--- src/lib/galleries.js
+++ src/lib/galleries.js
@@ -1,11 +1,13 @@
 import imageOne from "/public/gallery-1.jpg";
 import imageTwo from "/public/gallery-2.jpg";
 import imageThree from "/public/gallery-3.jpg";
+import imageFour from "/public/gallery-4.jpg";
 
 export const GALLERIES = () => {
   return [
     { title: "aurabalispa image", image: imageOne },
     { title: "aurabalispa image", image: imageTwo },
     { title: "aurabalispa image", image: imageThree },
+    { title: "aurabalispa image", image: imageFour },
   ];
 };
